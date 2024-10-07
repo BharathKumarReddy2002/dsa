1a3f51f51a94a77728b1d4ca3db749b4a56c25aa
Bharath Kumar Reddy <abharathreddy46174@gmail.com>
2024-10-08 04:16:49 +0530

    Update queueLinked.c

--- dsa/p1/queueLinked.c
+++ dsa/p1/queueLinked.c
@@ -1,104 +1,71 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct Node *f = NULL;
+struct Node *r = NULL;
+ 
+struct Node
+{
     int data;
-    struct node *next;
+    struct Node *next;
 };
-struct node *front,*rear;
-void insert_end_enque(int num){
-    struct node *temp;
-    temp = (struct node *)malloc(sizeof(struct node));
-    temp->data = num;
-    temp -> next = NULL;
-    if(front == NULL){
-        front = temp;
+ 
+void linkedListTraversal(struct Node *ptr)
+{
+    printf("Printing the elements of this linked list\n");
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+ 
+void enqueue(int val)
+{
+    struct Node *n = (struct Node *) malloc(sizeof(struct Node));
+    if(n==NULL){
+        printf("Queue is Full");
     }
     else{
-        rear->next = temp;
+        n->data = val;
+        n->next = NULL;
+        if(f==NULL){
+            f=r=n;
+        }
+        else{
+            r->next = n;
+            r=n;
+        }
     }
-    rear = temp;
 }
-int underflow(){
-    if(front == NULL){
-        return 1;
-    }
-    return 0;
-}
-int delete_front_deque(){
-    struct node *temp;
-    temp = front;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return;
-    }
-    int x;
-    x = temp->data;
-    front = front->next;
-    free(temp);
-    return x;
-}
-int peep(){
-    if(underflow()){
-        printf("list is empty\n");
-    }
-    return front->data;
-}
-void display(){
-    struct node *curr;
-    curr = front;
-    if(underflow()){
-        printf("list is empty\n");
+ 
+int dequeue()
+{
+    int val = -1;
+    struct Node *ptr = f;
+    if(f==NULL){
+        printf("Queue is Empty\n");
     }
     else{
-        while(curr != NULL){
-            printf("%d ",curr->data);
-            curr = curr->next;
-        }
-        printf("\n");
+        f = f->next;
+        val = ptr->data;
+        free(ptr);
     }
+    return val;
 }
-int main(){
-    while(1){
-        int choice,num;
-        printf("1. Insert a element in queue \n");
-        printf("2. delete a element in queue \n");
-        printf("3. peep element \n");
-        printf("4. display the queue \n");
-        printf("5. exit \n");
-        printf("Enter your choice\n");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1: 
-                printf("Element you want to insert \n");
-                scanf("%d",&num);
-                insert_end_enque(num);
-                break;
-            
-            case 2:
-                printf("Deleted element is %d",delete_front_deque());
-                break;
-            
-            case 3:
-                printf("Item at front of queue %d:",peep());
-                break;
-            
-            case 4:
-                display();
-                break;
-
-            case 5:
-                exit(1);
-        }
-    }
-    // insert_end_enque(20);
-    // insert_end_enque(10);
-    // insert_end_enque(30);
-    // insert_end_enque(40);
-    // display();
-    // int x,y;
-    // x = delete_front_deque();
-    // y = delete_front_deque();
-    // display();
-
+ 
+int main()
+{
+    linkedListTraversal(f);
+    printf("Dequeuing element %d\n", dequeue());
+    enqueue(34);
+    enqueue(4);
+    enqueue(7);
+    enqueue(17);
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    linkedListTraversal(f);
     return 0;
 }
