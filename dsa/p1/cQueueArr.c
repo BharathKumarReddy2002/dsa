cc59af6c92309e83e8fb203d2d68d5bd56e86d6a
Bharath Kumar Reddy <abharathreddy46174@gmail.com>
2024-10-08 04:14:49 +0530

    Update cQueueArr.c

--- dsa/p1/cQueueArr.c
+++ dsa/p1/cQueueArr.c
@@ -1,100 +1,76 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct cque{
-    int *c;
+ 
+struct circularQueue
+{
     int size;
-    int rear;
-    int front;
+    int f;
+    int r;
+    int* arr;
 };
-struct cque cq;
-int underflow(){
-    if(cq.rear == -1 && cq.front == -1){
+ 
+ 
+int isEmpty(struct circularQueue *q){
+    if(q->r==q->f){
         return 1;
     }
     return 0;
 }
-int overflow(){
-    if((cq.rear+1)%cq.size == cq.front){
+ 
+int isFull(struct circularQueue *q){
+    if((q->r+1)%q->size == q->f){
         return 1;
     }
     return 0;
 }
-void enque(int num){
-    if(!overflow()){
-        if(underflow()){
-            cq.front++;
-        }
-        cq.rear = (cq.rear+1)%cq.size;
-        cq.c[cq.rear] = num;
+ 
+void enqueue(struct circularQueue *q, int val){
+    if(isFull(q)){
+        printf("This Queue is full");
     }
     else{
-        printf("Queue is empty");
+        q->r = (q->r +1)%q->size;
+        q->arr[q->r] = val; 
+        printf("Enqued element: %d\n", val);
     }
 }
-int deque(){
-    int x = -999;
-    if(!underflow()){
-        x = cq.c[cq.front];
-        if(cq.front == cq.rear){
-            cq.front = -1;
-            cq.rear = -1;
-        }
-        //else{
-            cq.front = (cq.front+1)%cq.size;
-        
-    }
-    return x;
-
-}
-void display(){
-    int i;
-    if(!underflow()){
-        for(i= cq.front;i != cq.rear; i=(i+1)%cq.size){
-            printf("%d ",cq.c[i]);
-        }
-        printf("%d ",cq.c[i]);
+ 
+int dequeue(struct circularQueue *q){
+    int a = -1;
+    if(isEmpty(q)){
+        printf("This Queue is empty");
     }
     else{
-        printf("list is empty\n");
+        q->f = (q->f +1)%q->size;
+        a = q->arr[q->f]; 
     }
+    return a;
 }
+ 
+ 
 int main(){
-    printf("size:\n");
-    scanf("%d",&cq.size);
-    cq.c = (int *)malloc(sizeof(int)*cq.size);
-    cq.rear = cq.front = -1;
-    int ch,num,x;
-    while(1){
-        printf("1. Enque\n ");
-        printf("2. deque\n ");
-        printf("3. display\n ");
-        printf("4. Exit\n ");
-        printf("Enter choice\n");
-        scanf("%d",&ch);
-        switch(ch){
-            case 1:
-                printf("Enter number to insert\n");
-                scanf("%d",&num);
-                enque(num);
-                break;
-            case 2:
-                x = deque();
-                printf("deleting element %d\n",x);
-                break;
-            case 3:
-                 display();
-                //printf("deleting element%d\n",x);
-                break;
-            case 4:
-                exit(1);
-            default:
-                printf("Enter other elemnt");
-        }
+    struct circularQueue q;
+    q.size = 4;
+    q.f = q.r = 0;
+    q.arr = (int*) malloc(q.size*sizeof(int));
+    
+    // Enqueue few elements
+    enqueue(&q, 12);
+    enqueue(&q, 15);
+    enqueue(&q, 1); 
+    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q)); 
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+ 
+    if(isEmpty(&q)){
+        printf("Queue is empty\n");
     }
-    // enque(20);
-    // enque(50);
-    // display();
-
-
+    if(isFull(&q)){
+        printf("Queue is full\n");
+    }
+ 
     return 0;
 }
